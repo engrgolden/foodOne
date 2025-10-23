521d7582d1cf3b7e0116b8f98b4d30eb93163a89
Oderemi Boluwatife <oderemiboluwatife@gmail.com>
2025-10-23 11:27:42 +0100

    config

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
